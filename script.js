7957a2c8cfc45253d6d3718ddcb79ebca1abee10
Vaibhavbv <bansalv425@gmail.com>
2024-01-13 22:12:56 +0530

    good

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ let number = Math.floor(Math.random() * 100);
 btn.addEventListener('click', function(){
   let input = document.getElementById('userInput').value;
   if(input == number){
-    output.innerHTML = `you guessed it right , your number was ${number}`
+    output.innerHTML = `you guessed it right , your number was ${number},   Congratulations!!!`
   } else if (input < number){
     output.innerHTML = "You guessed too low!"
   };
